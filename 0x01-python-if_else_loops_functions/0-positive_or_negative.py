4c4aef9f2ecddf56ebad661d986ad17f7c443cff
Alfred <alfredphronesis@outlook.com>
2023-10-31 14:32:13 -0700

    Fixed the errors involved

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = number[-1]
+last_digit = str(number)[-1]
+
+last_digit = int(last_digit)
 
 if last_digit > 5:
     print(f"Last digit of{number} is {last_digit}\
@@ -14,4 +16,3 @@ elif last_digit == 0:
 elif last_digit < 6 and last_digit != 0:
     print(f"Last digit of{number} is {last_digit}\
           and is less than 6 and not 0")
-
