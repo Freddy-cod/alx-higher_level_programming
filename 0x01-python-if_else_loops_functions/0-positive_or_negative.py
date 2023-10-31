b2ca96f890b9edd79414db5d0b9313d75d5608f5
Alfred <alfredphronesis@outlook.com>
2023-10-31 16:05:00 -0700

    Fixed the errors

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
+number = random.randint(-10, 10)
 
-last_digit = str(number)[-1]
+if number > 0:
+    print(f"{number} is positive")
 
-last_digit = int(last_digit)
+elif number == 0:
+    print(f"{number} is zero")
 
-if last_digit > 5:
-    print(f"Last digit of{number} is {last_digit}\
-    and is greater than 5")
-
-elif last_digit == 0:
-    print(f"Last digit of{number} is {last_digit}\
-    and is 0")
-elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of{number} is {last_digit}\
-    and is less than 6 and not 0")
+elif number < 0:
+    print(f"{number} is negative")
---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+last_digit = str(number)[-1]
+
+last_digit = int(last_digit)
+
+if last_digit > 5:
+    print(f"Last digit of{number} is {last_digit}\
+    and is greater than 5")
+
+elif last_digit == 0:
+    print(f"Last digit of{number} is {last_digit}\
+    and is 0")
+elif last_digit < 6 and last_digit != 0:
+    print(f"Last digit of{number} is {last_digit}\
+    and is less than 6 and not 0")
