b6a0e4839b9fb44f00d5f6d755b3f923155bb704
Alfred <alfredphronesis@outlook.com>
2023-10-31 14:38:56 -0700

    Fixed the errors I guess

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -8,11 +8,11 @@ last_digit = int(last_digit)
 
 if last_digit > 5:
     print(f"Last digit of{number} is {last_digit}\
-          and is greater than 5")
+    and is greater than 5")
 
 elif last_digit == 0:
     print(f"Last digit of{number} is {last_digit}\
-          and is 0")
+    and is 0")
 elif last_digit < 6 and last_digit != 0:
     print(f"Last digit of{number} is {last_digit}\
-          and is less than 6 and not 0")
+    and is less than 6 and not 0")
