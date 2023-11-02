841a00a9510510cd38cddaa4578f2cb26953c2f1
Alfred <alfredphronesis@outlook.com>
2023-11-01 22:34:44 -0700

    errors fixed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,13 +4,12 @@
 from add_0 import add
 
 if __name__ == "__main__":
-    # Declare two variables, num1 and num2, and assign them the values 1 and 2.
-    num1 = 1
-    num2 = 2
+    # Declare two variables, a and b, and assign them the values 1 and 2.
+    a = 1
+    b = 2
 
     # Add the two numbers together and store the result in the variable result.
-    result = add(num1, num2)
+    result = add(a, b)
 
     # Print the result of the addition to the console.
-    print("{} + {} = {}".format(num1, num2, result))
-
+    print("{} + {} = {}".format(a, b, result))
