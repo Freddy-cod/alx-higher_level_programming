d5d7f4922dd72b00850d0252b20cd95926ee8515
Alfred <alfredphronesis@outlook.com>
2023-11-05 11:33:32 -0800

    task added

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@ def print_list_integer(my_list=[]):
     # Iterate through the elements in the input list
     for num in my_list:
         # Print each integer using str.format()
-        print("{}".format(num))
+        print("{:d}".format(num))
