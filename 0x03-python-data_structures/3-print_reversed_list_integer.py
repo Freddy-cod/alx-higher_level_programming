c8093f518f862b1f3387429277c279dbedbde022
Alfred <alfredphronesis@outlook.com>
2023-11-05 11:46:23 -0800

    fourth task added

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    # Iterate through the elements in the reversed list
+    for num in reversed(my_list):
+        print("{}".format(num))
