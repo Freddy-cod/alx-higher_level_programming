9bc9759fa442b6ef9192fcfc05f6ea0efa13a3e5
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2023-11-05 22:09:59 +0200

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # Iterate through the rows of the matrix
-    for row in matrix:
-        # Iterate through the elements in each row
-        for element in row:
-            # Print each element with formatting
-            print("{:d}".format(element), end=" ")
-        print()  # Move to the next line after each row
+    for nums in matrix:
+        print(' '.join("{:d}".format(digit) for digit in nums))
