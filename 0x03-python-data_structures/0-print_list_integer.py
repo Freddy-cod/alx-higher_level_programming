c425187c6b15c49b24b6429af94437337147c104
Alfred <alfredphronesis@outlook.com>
2023-11-05 11:23:25 -0800

    first task

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    # Iterate through the elements in the input list
+    for num in my_list:
+        # Print each integer using str.format()
+        print("{}".format(num))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+Data strucutures in Python
