1a8df4130ce83a563de065a9fd72a91b61496277
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2023-11-08 09:14:29 +0200

    Update 101-easy_print.py

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,12 +1,2 @@
-def magic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.sub
-
-    if a < b:
-        c = add(a, b)
-
-        for i in range(4, 6):
-            c = add(c, i)
-
-        return c
-
-    return sub(a, b)
+#!/usr/bin/python3
+import easy_print_101
