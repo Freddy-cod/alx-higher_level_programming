94dc1cff4b718bc1a9b98d718f40bd39ab544163
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2023-11-08 08:42:05 +0200

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# This code adds two numbers together and prints the result.
 from add_0 import add
 
 if __name__ == "__main__":
-    # Declare two variables, a and b, and assign them the values 1 and 2.
     a = 1
     b = 2
-    # Print the result of the addition to the console.
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    
