2f936df59d083a01db3834356251f31c7851bc0f
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2023-11-08 08:54:44 +0200

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-# Import arithmetic functions from calculator_1.
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
-    # Declare variables a and b.
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-
-    # Perform basic arithmetic operations and print results.
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
