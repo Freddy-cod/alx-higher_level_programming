43334ac8ee9d2c29dcb57ac53225c5c73cae6f82
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2023-11-10 17:25:44 +0200

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())  # Sort the keys alphabetically
-    
-    for key in sorted_keys:
-        value = a_dictionary[key]
-        
-        if isinstance(value, dict):
-            # If the value is another dictionary, print its keys without sorting
-            print(key, ": {")
-            for subkey, subvalue in value.items():
-                print(f"    {subkey}: {subvalue}")
-            print("}")
-        else:
-            # Print the key and its value
-            print(key, ":", value)
+    for key in sorted(a_dictionary):
+        value = a_dictionary.get(key)
+        print('{}: {}'.format(key, value))
