c74ece6d2a355396b51c0211adf3ecc502f9f161
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-03-19 16:06:11 +0200

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,19 +8,19 @@
  */
 int check_cycle(listint_t *list)
 {
-        listint_t *slow = list;
-        listint_t *fast = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-        if (!list)
-                return (0);
+	if (!list)
+		return (0);
 
-        while (slow && fast && fast->next)
-        {
-                slow = slow->next;
-                fast = fast->next->next;
-                if (slow == fast)
-                        return (1);
-        }
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
 
-        return (0);
+	return (0);
 }
