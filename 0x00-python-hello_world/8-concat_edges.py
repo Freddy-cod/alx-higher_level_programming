49ffb6e0d3fcbb3c97a5cb8168cec61090d860bc
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-03-19 15:56:34 +0200

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+
 print("object-oriented programming with Python")
+print(str)
