aa7f33dfb5ee95d9e4e38c2764dbd0f266533bc3
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-03-19 17:46:13 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
 
-# Check if the script is being run as the main program
-if __name__ != "__main__":
-    exit()
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
 
-# Calculate the number of command-line arguments
-argc = len(argv) - 1
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-# Check if the correct number of arguments is provided
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-elif argv[2] == '+':
-    func = add
-elif argv[2] == '-':
-    func = sub
-elif argv[2] == '*':
-    func = mul
-elif argv[2] == '/':
-    func = div
-else:
-    print("Unknown operator. Available operators: +, -, *, and /")
-    exit(1)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-# Perform the calculation using the selected operator function
-result = func(int(argv[1]), int(argv[3]))
-
-# Display the calculation result
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2],
-                                     int(argv[3]), result))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
