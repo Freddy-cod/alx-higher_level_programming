226c1a8c4a906775e5e7a91f4e8002cf6f9f431a
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-04-03 12:00:24 +0200

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    # Handle type and value errors. 
+    # Handle type and value errors.
     num = 0
     for items in range(x):
         try:
