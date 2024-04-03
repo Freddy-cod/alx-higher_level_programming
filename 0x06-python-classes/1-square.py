d51a4edd526385db9de775b9d84263046978b51f
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-04-03 18:43:30 +0200

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""My Square class."""
+
+
+class Square:
+    """Represent my square."""
+
+    def __init__(self, size):
+        """Initialization of variables.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
