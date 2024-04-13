b487f35ab563c1f5461ef16fc73a6cb745d5b2ca
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-04-13 15:38:08 +0200

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,14 @@
+#!/usr/bin/python3
+
+"""My functions for adding two numbers."""
+
+
 def add_integer(a, b=98):
-    try:
-        num = int(a)
-        num2 = int(b)
-    except TypeError(
-    
+    """Outputs the addition of a and b.
+       Raises an error if a and b aint numbers.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
