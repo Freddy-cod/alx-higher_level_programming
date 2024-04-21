6d05c4ed79e528f8d1f8bccf16702a70ce2cc17c
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-04-21 12:00:26 +0200

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+=============================
+Returns attrs of an object
+=============================
+"""
+
+
+def lookup(obj):
+    """Function for return the attributes for an object"""
+
+    return dir(obj)
