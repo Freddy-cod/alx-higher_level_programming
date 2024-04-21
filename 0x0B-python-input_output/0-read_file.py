be340b0fe6c739981bdaa0949866f145fc368c64
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-04-21 14:01:32 +0200

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    """function that reads a text file and prints it"""
 
-    with open(filename) as f:
-        text = f.read()
-        print(text, end="")
+
+def read_file(filename=""):
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
