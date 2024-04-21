db48f3d5d4054b4f19fbac8172e299bc261e180b
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-04-21 12:05:03 +0200

    Create 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+===========================
+Sort the data.
+===========================
+"""
+
+
+class MyList(list):
+    """Class with method print_sorted"""
+    pass
+
+    def print_sorted(self):
+        """Methot that sorted a list"""
+
+        print(sorted(list(self)))
