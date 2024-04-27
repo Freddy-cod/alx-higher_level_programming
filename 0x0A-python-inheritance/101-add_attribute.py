416a83e45d6e962b8d18dcf1b32bb02c13d9049c
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-04-27 09:19:34 +0200

    Create 101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+method module
+"""
+
+
+def add_attribute(obj, objname, value):
+    """add attribute to object
+    args:
+        obj: class object
+        objname: object name
+        value: value of attribute
+    return:
+        na
+    """
+    if hasattr(obj, "__dict__"):
+        setattr(obj, objname, value)
+    else:
+        raise TypeError("can't add new attribute")
