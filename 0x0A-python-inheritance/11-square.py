1eae36aeff43b4ffe175d08bd8ea189e64410f0c
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-04-27 09:16:34 +0200

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
+"""This modle creates a class named Square"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle that inherits BaseGeometry"""
-
+    """A class named Square
+    Attributes:
+    attr1(size): size of square
+    attr2(area): finds the area of it
+    """
     def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
-        super().__init__(size, size)
+        """Initializes an instance"""
         self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
+        super().__init__(size, size)
 
     def __str__(self):
+        """Returns informal string representation"""
         return "[Square] {}/{}".format(self.__size, self.__size)
