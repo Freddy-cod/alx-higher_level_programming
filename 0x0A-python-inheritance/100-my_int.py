73bc9050bed0cf2ca4cb80f8e35a5f4dc09e28e8
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-04-27 09:18:14 +0200

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,40 +1,18 @@
 #!/usr/bin/python3
 """
-=============================
-Module with a class Myint
-=============================
+This module creates a class named MyInt
 """
 
 
 class MyInt(int):
-    """class that inherits from int"""
-
-    def __init__(self, my_int):
-        """for initialize a value my_int"""
-
-        self.my_int = my_int
-
-    @property
-    def my_int(self):
-        return self.__my_int
-
-    @my_int.setter
-    def my_int(self, my_int):
-        if type(my_int) is not int:
-            raise TypeError("my_int must be an integer")
-        else:
-            self.__my_int = my_int
+    """
+    A class named MyInt
+    """
 
     def __eq__(self, other):
-        """equal method"""
-
-        if self.my_int == other:
-            return False
-        else:
-            return True
+        """Swaps the eq builtin"""
+        return int.__ne__(self, other)
 
     def __ne__(self, other):
-            if self.my_int != other:
-                return False
-            else:
-                return True
+        """Swaps the ne builtin"""
+        return int.__eq__(self, other)
