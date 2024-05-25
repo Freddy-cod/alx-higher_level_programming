31782d8318640fe2d6a5a3681f63aba01aad76e1
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-05-25 11:29:51 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,41 +1,37 @@
 #!/usr/bin/python3
+'''Module for log parsing script.'''
+import sys
 
-"""
-Module that reads stdin line by line and computes metrics
-"""
+if __name__ == "__main__":
+    size = [0]
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-if __name__ == '__main__':
+    def check_match(line):
+        '''Checks for regexp match in line.'''
+        try:
+            line = line[:-1]
+            words = line.split(" ")
+            size[0] += int(words[-1])
+            code = int(words[-2])
+            if code in codes:
+                codes[code] += 1
+        except:
+            pass
 
-    import sys
-
-    file_size = 0
-    possible_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {i: 0 for i in possible_codes}
-    counter = 0
-
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(file_size))
-        for i, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(i, v))
-
+    def print_stats():
+        '''Prints accumulated statistics.'''
+        print("File size: {}".format(size[0]))
+        for k in sorted(codes.keys()):
+            if codes[k]:
+                print("{}: {}".format(k, codes[k]))
+    i = 1
     try:
         for line in sys.stdin:
-            counter += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                file_size += int(data[-1])
-            except BaseException:
-                pass
-            if counter % 10 == 0:
-                print_stats(stats, file_size)
-        print_stats(stats, file_size)
+            check_match(line)
+            if i % 10 == 0:
+                print_stats()
+            i += 1
     except KeyboardInterrupt:
-        print_stats(stats, file_size)
+        print_stats()
         raise
+    print_stats()
