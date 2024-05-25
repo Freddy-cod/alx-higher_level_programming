8853c5f48986377e21d9cefc7e5c158553a5a0ea
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-05-25 11:18:53 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-
-
 import json
+"""JavaScript Object Notation"""
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, "w", encoding="UTF-8") as f:
-        json.dump(my_obj, f)
+    """writes an Object to a text file, using a JSON representation
+    Args:
+        my_obj(obj): object
+        filename(str): filename
+    """
+    with open(filename, mode="w", encoding="utf-8") as a_file:
+        a_file.write(json.dumps(my_obj))
