c61ae58f3835e81b59f4d3b42f4c813799aa0959
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-05-25 10:14:47 +0200

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
+"""
+prints "My name is"
+"""
+
+
 def say_my_name(first_name, last_name=""):
-    if not isinstance(first_name, str):
+    """prints "My name is"
+
+    Args:
+        first_name (str): first name
+        last_name (str): last name
+
+    Raises:
+        TypeError: if either first_name or last_name are not strings
+    """
+
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {:s} {:s}".format(first_name, last_name))
