9babb2da9445f199257987f8e0795439e7681489
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-05-28 17:09:46 +0200

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,7 @@
 -- converts hbtn_0c_0 database to utf8
+-- table: first_table to utf8
+-- field: name to utf8
+
 ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
