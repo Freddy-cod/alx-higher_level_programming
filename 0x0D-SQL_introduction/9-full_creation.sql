8b0fd10acdbf8032bedd04aeaef47723d3730b2c
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-05-28 17:00:30 +0200

    Create 9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+--  Create a Second table and insert data
+CREATE table IF NOT EXISTS second_table (`id` INT,
+`name` VARCHAR(256),
+`score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`)
+VALUES
+       (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
