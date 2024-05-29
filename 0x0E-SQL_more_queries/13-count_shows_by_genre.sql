34db85c854322fe3a9a8ce273704cf8250848a8f
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-05-29 11:41:48 +0200

    Create 13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- lists all genres contained in hbtn_0d_ and the number of shows linked to each
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
