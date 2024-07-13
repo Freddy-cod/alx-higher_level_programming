8158b28e168a94937781b43bc24ada01248c85ca
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-07-13 18:33:28 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
 import sys
 import MySQLdb
+'''Usage: ./0-select_states.py <mysql username>'''
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
