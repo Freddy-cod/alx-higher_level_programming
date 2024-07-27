064fe1db8aa545f586a1132462f129f8204d907d
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-07-27 17:29:33 +0200

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Store the URL from the first argument
-URL=$1
-
-# Send GET request with header and display the response body
-curl -s -H "X-School-User-Id: 98" "$URL"
+# script to send custom headers to servers
+curl -s -H "X-Hello School!-User-Id: 98" "$1"
