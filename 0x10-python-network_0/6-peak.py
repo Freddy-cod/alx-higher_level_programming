edcd2552d305cf22923adab022da9c068ee877d2
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-08-09 14:10:03 +0200

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-"""find the max"""
+"""find the peak"""
 
 
 def find_peak(list_of_integers):
-    """find_peak"""
-    if not len(list_of_integers):
+    '''Find Peak'''
+    if not list_of_integers:
         return None
-    else:
-        return max(list_of_integers)
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
