6d8565dac77b37d74886b6d59ef11149fd6a57c5
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-08-09 14:01:36 +0200

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # script to send custom headers to servers
-curl -s -H "X-Hello School!-User-Id: 98" "$1"
+#!/bin/bash
+# script to send custom headers to servers
+curl -s -H "X-School-User-Id: 98" "$1"
