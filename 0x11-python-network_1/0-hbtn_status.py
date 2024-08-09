4914c7f8ee006c5c767f2aa2d9cb190582a53f92
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-08-09 14:22:31 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,6 @@ import urllib.request
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     request = urllib.request.Request(url)
-    
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
