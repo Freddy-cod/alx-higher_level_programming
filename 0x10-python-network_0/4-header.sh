1de46b56e0c2423cf40ecad0c0d4626bb6f66e61
Freddy-cod <alfredphronesis@outlook.com>
2024-08-09 04:33:18 -0700

    rquest headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if the URL is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# URL provided as an argument
-URL="$1"
-
-# Send a GET request to the URL with the custom header and display the response body
-curl -s -H "X-School-User-Id: 98" "$URL"
+# script to send custom headers to servers
+curl -s -H "X-Hello School!-User-Id: 98" "$1"
